9504c600024a53c969ccd40f712ef4e23f962c63
Tim Golding <tgolding55@hotmail.com>
2019-09-20 14:57:44 +0100

    Done.

--- run.rb
+++ run.rb
@@ -6,5 +6,13 @@ require 'pry'
 #Test your code here
 
 
+zoo1 = Zoo.new("Zoo1", "France")
+zoo2 = Zoo.new("Zoo2", "Spain")
+zoo3 = Zoo.new("Zoo3", "Germany")
+
+animal1 = Animal.new("Lion", 10, "Leo", zoo1)
+animal2 = Animal.new("Cow", 20, "John", zoo1)
+animal3 = Animal.new("Cow", 15, "joan", zoo2)
+
 binding.pry
 puts "done"
