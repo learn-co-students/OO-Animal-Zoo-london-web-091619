5a0b4ffc8a2568b13dc93d847d99e58fd45940d2
idmyn <hello@davidmyno.rs>
2019-09-22 07:23:30 +0100

    Done

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,25 @@
 class Animal
+  attr_accessor :weight, :zoo
+  attr_reader :nickname, :species
 
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def initialize(species:, weight:, nickname:)
+    @species = species
+    @weight = weight
+    @nickname = nickname
+    save
+  end
+
+  def self.find_by_species(species)
+    all.select { |animal| animal.species.eql?(species) }
+  end
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,43 @@
 class Zoo
+  attr_reader :name, :location
 
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_location(location)
+    all.select { |zoo| zoo.location.eql?(location) }
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def initialize(name:, location:)
+    @name = name
+    @location = location
+    save
+  end
+
+  def adopt_animal(animal)
+    animal.zoo = self
+  end
+
+  def animals
+    Animal.all.select { |animal| animal.zoo.eql?(self) }
+  end
+
+  def animal_species
+    animals.map(&:species).uniq
+  end
+
+  def animal_nicknames
+    animals.map(&:nickname)
+  end
+
+  def find_by_species(species)
+    animals.select { |animal| animal.species.eql?(species) }
+  end
 end
