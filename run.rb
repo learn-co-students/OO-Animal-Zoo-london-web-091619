af425277bf2a7426c43ec939255d558a0b68b7a0
github username <github email address>
2019-10-16 15:00:55 +0100

    done

--- run.rb
+++ run.rb
@@ -5,6 +5,14 @@ require 'pry'
 
 #Test your code here
 
+z1 = Zoo.new("zoolology", "london")
+z2 = Zoo.new("biology", "reading")
+
+a1 = Animal.new("dog", 40, "Buster", z1)
+a2 = Animal.new("dog", 50, "Snoop", z2)
+a3 = Animal.new("cat", 33, "Tom Jones", z1)
+
+
 
 binding.pry
 puts "done"
